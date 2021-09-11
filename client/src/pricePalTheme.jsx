b1f26afc1ee6d018c9663921d6e580b94a511bf1
Mark Lin <mark.lin2234@gmail.com>
2021-09-10 20:54:29 -0400

    first commit

---
+++ client/src/pricePalTheme.jsx
@@ -0,0 +1,52 @@
+import { createTheme } from "@material-ui/core";
+
+const PRIMARY_COLOR = "#e2a8fe";
+const SECONDARY_COLOR = "#000000";
+const TEXT_COLOR_DARK = "#4b4b4b";
+const TEXT_COLOR_LIGHT = "#767676";
+
+export const pricePalTheme = createTheme({
+  palette: {
+    primary: {
+      main: PRIMARY_COLOR,
+    },
+    secondary: {
+      main: SECONDARY_COLOR,
+    },
+  },
+  typography: {
+    fontFamily: '"Assistant", "Helvetica", "Arial", sans-serif',
+    h1: {
+      color: TEXT_COLOR_DARK,
+      fontSize: "1.5rem",
+    },
+    h2: {
+      color: TEXT_COLOR_DARK,
+      fontSize: "1.25rem",
+    },
+    h3: {
+      color: TEXT_COLOR_DARK,
+      fontSize: "1.1rem",
+    },
+    h5: {
+      color: SECONDARY_COLOR,
+      fontSize: 16,
+    },
+    subtitle1: {
+      color: PRIMARY_COLOR,
+      fontSize: "1.25rem",
+      fontWeight: 600,
+    },
+    subtitle2: {
+      color: SECONDARY_COLOR,
+      fontSize: "1.1rem",
+      fontWeight: 600,
+    },
+    body1: {
+      color: TEXT_COLOR_DARK,
+    },
+    body2: {
+      color: TEXT_COLOR_LIGHT,
+    },
+  },
+});
