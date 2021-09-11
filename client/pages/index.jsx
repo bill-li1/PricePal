15e99dfa982b1d00712d107c1de7cc61d3b2ee94
Mark Lin <mark.lin2234@gmail.com>
2021-09-11 15:46:56 -0400

    added _document.jsx

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,45 +1,45 @@
 import { Grid, makeStyles, TextField, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-	image: {
-		maxWidth: '100%',
-	},
-	login: {
-		marginTop: 'auto',
-		marginBottom: 'auto',
-		marginLeft: theme.spacing(3),
-	},
+  image: {
+    maxWidth: '100%',
+  },
+  login: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    marginLeft: theme.spacing(3),
+  },
 }));
 export default function Home() {
-	const styles = useStyles();
-	return (
-		<main>
-			<Grid container direction="row">
-				<Grid item xs={12} md={4}>
-					<img className={styles.image} alt="logo" src="/mockup.svg" />
-				</Grid>
-				<Grid item xs={12} md={6} className={styles.login}>
-					<Grid container direction="column" spacing={2}>
-						<Grid item>
-							<Typography variant="h1">Pay your way.</Typography>
-						</Grid>
-						<Grid item>
-							<TextField
-								variant="outlined"
-								placeholder="john.doe"
-								label="username"
-							/>
-						</Grid>
-						<Grid item>
-							<TextField
-								variant="outlined"
-								placeholder="*********"
-								label="password"
-							/>
-						</Grid>
-					</Grid>
-				</Grid>
-			</Grid>
-		</main>
-	);
+  const styles = useStyles();
+  return (
+    <main>
+      <Grid container direction="row">
+        <Grid item xs={12} md={4}>
+          <img className={styles.image} alt="logo" src="/mockup.svg" />
+        </Grid>
+        <Grid item xs={12} md={6} className={styles.login}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <Typography variant="h1">Pay your way.</Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                variant="outlined"
+                placeholder="john.doe"
+                label="username"
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                variant="outlined"
+                placeholder="*********"
+                label="password"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </main>
+  );
 }
