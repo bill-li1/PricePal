7e48e08eaa6539b2381e5465485cfbc1d3d2334c
MarkLinXVII <34454613+MarkLinXVII@users.noreply.github.com>
2021-09-18 22:48:03 -0400

    App dashboard (#2)

--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -1,7 +1,48 @@
-export default function dashboard() {
+import { makeStyles } from '@material-ui/core';
+import { CreateGroupButton } from 'components/CreateGroupButton';
+import { CreateGroupForm } from 'components/CreateGroupForm';
+import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: 1,
+  },
+  button: {
+    position: 'sticky',
+    bottom: 16,
+    float: 'right',
+    zIndex: 100,
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      marginRight: theme.spacing(30),
+      bottom: '3%',
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginRight: theme.spacing(35),
+    },
+    [theme.breakpoints.up('xl')]: {
+      marginRight: theme.spacing(20),
+    },
+  },
+}));
+
+export default function Dashboard() {
+  const styles = useStyles();
+  const [createGroup, setCreateGroup] = useState(false);
+
+  const handleCreateGroupClick = () => {
+    setCreateGroup(true);
+  };
+  const handleCloseCreateGroup = () => {
+    setCreateGroup(false);
+  };
   return (
-    <section>
+    <section className={styles.root}>
       <h1>Dashboard</h1>
+      <div className={styles.button}>
+        <CreateGroupButton onClick={handleCreateGroupClick} />
+      </div>
+      <CreateGroupForm open={createGroup} onClose={handleCloseCreateGroup} />
     </section>
   );
 }
